4159a1575f972cc549151fbbd61ffe50892564b8
Willem Liu <willem_liu@hotmail.com>
2019-02-11 16:39:00 +0100

    sourcemap for development bjuild

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,13 @@
 
 const webpack = require("webpack");
 const path = require('path');
+const NODE_ENV = JSON.stringify(process.env.NODE_ENV ? process.env.NODE_ENV : 'development');
+const devtool = NODE_ENV == '"development"' ? 'source-map' : undefined;
 
 console.info('process.env.NODE_ENV', JSON.stringify(process.env.NODE_ENV));
 
 module.exports = {
-    // devtool: "source-map",
+    devtool,
     entry: {
         "typography": ["./src/typography.tsx"]
     },
